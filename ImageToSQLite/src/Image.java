19a50496e49ca67c21a3ecce4a4c40be2a6b98a2
Afiqur Rahman Piash <afiqur_rahman@yahoo.com>
2018-05-26 00:34:56 +0600

    2nd Commit

--- ImageToSQLite/src/Image.java
+++ ImageToSQLite/src/Image.java
@@ -33,22 +33,19 @@ public class Image extends JFrame {
 	private JPanel contentPane;
 	JLabel label;
 	JDesktopPane desktopPane;
-	
-	
-	
+
 	String ss;
-	byte [] image_detail = null;
+	byte[] image_detail = null;
 	private ImageIcon format = null;
-	String  filename = null;
+	String filename = null;
 	int s = 0;
 	byte[] image = null;
 	AbstractButton path;
 
-	
 	/**
 	 * Launch the application.
 	 */
-	
+
 	public static void main(String[] args) {
 		EventQueue.invokeLater(new Runnable() {
 			public void run() {
@@ -65,52 +62,48 @@ public class Image extends JFrame {
 	/**
 	 * Create the frame.
 	 */
-	
+
 	Connection conn = null;
 	private JButton btnAdd;
 	private JButton btnShowImage;
-	
+
 	public Image() {
-		
-		
-		
+
 		conn = SqliteConnection.dbConnector();
-		
+
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		setBounds(100, 100, 450, 300);
 		contentPane = new JPanel();
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
 		setContentPane(contentPane);
 		contentPane.setLayout(null);
-		
+
 		desktopPane = new JDesktopPane();
 		desktopPane.setBounds(20, 11, 200, 140);
 		contentPane.add(desktopPane);
-		
+
 		label = new JLabel("");
 		label.setBounds(0, 0, 200, 140);
 		desktopPane.add(label);
-		
-		
+
 		JButton btnSelectImage = new JButton("Select Image");
 		btnSelectImage.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
-				
+
 				JFileChooser fileChooser = new JFileChooser();
-		         fileChooser.setCurrentDirectory(new File(System.getProperty("user.home")));
-		         FileNameExtensionFilter filter = new FileNameExtensionFilter("*.IMAGE", "jpg","gif","png");
-		         fileChooser.addChoosableFileFilter(filter);
-		         int result = fileChooser.showSaveDialog(null);
-		         if(result == JFileChooser.APPROVE_OPTION){
-		             File selectedFile = fileChooser.getSelectedFile();
-		             try {
+				fileChooser.setCurrentDirectory(new File(System.getProperty("user.home")));
+				FileNameExtensionFilter filter = new FileNameExtensionFilter("*.IMAGE", "jpg", "gif", "png");
+				fileChooser.addChoosableFileFilter(filter);
+				int result = fileChooser.showSaveDialog(null);
+				if (result == JFileChooser.APPROVE_OPTION) {
+					File selectedFile = fileChooser.getSelectedFile();
+					try {
 						@SuppressWarnings("resource")
 						FileInputStream fis = new FileInputStream(selectedFile);
 						ByteArrayOutputStream bos = new ByteArrayOutputStream();
-						
-						byte [] buf = new byte[1024];
-						for(int readNum; (readNum=fis.read(buf))!= -1;)
-						{
+
+						byte[] buf = new byte[1024];
+						for (int readNum; (readNum = fis.read(buf)) != -1;) {
 							bos.write(buf, 0, readNum);
 						}
 						image_detail = bos.toByteArray();
@@ -119,13 +112,11 @@ public class Image extends JFrame {
 					} catch (IOException e1) {
 						e1.printStackTrace();
 					}
-		             
-		             
-		              }
-		         else if(result == JFileChooser.CANCEL_OPTION){
-		             System.out.println("No Data");
-		         }
-		         
+
+				} else if (result == JFileChooser.CANCEL_OPTION) {
+					System.out.println("No Data");
+				}
+
 				btnSelectImage.setText(filename);
 			}
 		});
@@ -133,24 +124,23 @@ public class Image extends JFrame {
 		btnSelectImage.setFont(new Font("Tahoma", Font.ITALIC, 14));
 		btnSelectImage.setBounds(256, 23, 130, 43);
 		contentPane.add(btnSelectImage);
-		
+
 		btnAdd = new JButton("Insert");
 		btnAdd.setFont(new Font("Tahoma", Font.PLAIN, 13));
 		btnAdd.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
-				try
-				{
+				try {
 					String query = " insert into ImageTable (Image) values (?)";
 					PreparedStatement pst = conn.prepareStatement(query);
-			
+
 					pst.setBytes(1, image_detail);
-			
+
 					pst.execute();
-					
+
 					JOptionPane.showMessageDialog(null, "Image Inserted");
-					
+
 					pst.close();
-					
+
 				} catch (Exception es) {
 					es.printStackTrace();
 				}
@@ -158,34 +148,32 @@ public class Image extends JFrame {
 		});
 		btnAdd.setBounds(277, 77, 89, 23);
 		contentPane.add(btnAdd);
-		
+
 		btnShowImage = new JButton("Show Image");
 		btnShowImage.setFont(new Font("Sylfaen", Font.PLAIN, 14));
 		btnShowImage.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
-				
-					try{
-						String query = "select * from ImageTable Where rowid=1";
-						PreparedStatement pst=conn.prepareStatement(query);
-						ResultSet rs = pst.executeQuery();
-						
-						while(rs.next())
-						{
-						byte[]imagedata = rs.getBytes("Image");
-						format =  new ImageIcon (imagedata);
+
+				try {
+					String query = "select * from ImageTable Where rowid=1";
+					PreparedStatement pst = conn.prepareStatement(query);
+					ResultSet rs = pst.executeQuery();
+
+					while (rs.next()) {
+						byte[] imagedata = rs.getBytes("Image");
+						format = new ImageIcon(imagedata);
 						label.setIcon(format);
-						}
-						pst.close();		
 					}
-					catch (Exception ef){
+					pst.close();
+				} catch (Exception ef) {
 					ef.printStackTrace();
-					}
+				}
 			}
 		});
-		
+
 		btnShowImage.setBounds(256, 111, 130, 25);
 		contentPane.add(btnShowImage);
-		
+
 		JLabel lblImageSize = new JLabel("Image Size : 200*140");
 		lblImageSize.setFont(new Font("Tahoma", Font.PLAIN, 13));
 		lblImageSize.setBounds(30, 159, 139, 23);
